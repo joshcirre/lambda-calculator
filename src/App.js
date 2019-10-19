261f0672c24b14b9eec4288b85ee751f07d4639d
Josh Cirre <joshcirre@gmail.com>
2019-10-19 10:27:30 -0700

    added imports to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+import Displays from './components/DisplayComponents/Display';
+import { numbers, operator, specials } from './data';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
-import Logo from "./components/DisplayComponents/Logo";
+import Logo from './components/DisplayComponents/Logo';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
